4804ebab2a66ecc66bab0093ff3c10d6cf4519a5
SinghAvi <avinashmca2014@gmail.com>
2019-08-06 08:53:46 +0530

    adding image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_and_belongs_to_many :roles
+  has_one_attached :avatar
   validates_format_of :email, :with => /\A[a-zA-Z0-9\._'-]+@([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,4}\z/, :allow_blank => true, :multiline => true
   validates_uniqueness_of :email
 end
